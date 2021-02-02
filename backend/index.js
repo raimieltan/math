338da94a95aeb0b03a52da51c719552c65c9f1d2
Michael Handumon <kmcaelian@gmail.com>
2021-02-02 19:22:18 +0800

    problem route fix

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(cors())
 app.use(bodyParser.urlencoded( {extended: true }))
 
 app.use('/auth', require("./routes/authentication.js"))
-app.use('/auth', require("./routes/problems.js"))
+app.use('/problem', require("./routes/problems.js"))
 
 const PORT = 8000
 
