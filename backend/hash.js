6e0658490f843635f858bc2293e8d46c78639210
Michael Handumon <kmcaelian@gmail.com>
2021-02-09 14:16:23 +0800

    updated codes

--- backend/hash.js
+++ backend/hash.js
@@ -5,9 +5,9 @@ pool.connect((error, client) => {
   client.query('SELECT * FROM users')
     .then((results) => {
       for (const user of results.rows) {
-        bcrypt.hash(user.user_password, 12)
+        bcrypt.hash(user.password, 12)
           .then((hash) => {
-            client.query('UPDATE users SET user_password = $1 WHERE user_id = $2', [hash, user.user_id])
+            client.query('UPDATE users SET password = $1 WHERE id = $2', [hash, user.id])
           })
           .catch((error) => {
             console.log("Error hashing password: ", error);
