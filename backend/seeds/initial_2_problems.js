1e2ea1e823fb935d69424ef31f4488815ac67d8f
raimieltan <raimieltan@gmail.com>
2021-02-14 21:34:20 +0800

    added choices seeds

--- backend/seeds/initial_2_problems.js
+++ backend/seeds/initial_2_problems.js
@@ -6,5 +6,11 @@ exports.seed = function (knex) {
       '1 + 5 = 6', 
       '6'
     )
+
+    INSERT INTO choices VALUES(
+      DEFAULT,
+      '6',
+      1
+    )
   `)
 };
---
+++ backend/seeds/initial_3_choices.js
@@ -0,0 +1,31 @@
+exports.seed = function (knex) {
+    return knex.raw(`
+
+  
+      INSERT INTO choices VALUES(
+        DEFAULT,
+        '6',
+        1
+      ),
+
+      (
+        DEFAULT,
+        '7',
+        1
+      ),
+
+      (
+        DEFAULT,
+        '8',
+        1
+      ),
+
+      (
+        DEFAULT,
+        '9',
+        1
+      ),
+
+    `)
+  };
+  
